9b4d74259583f925e067750816c3a74942536323
BibianaGarcia <63804813+BibianaGarcia@users.noreply.github.com>
2021-08-03 18:05:11 -0300

    demo update

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function toggleNavbar(){
+  document.getElementById("navbar-links").classList.toggle("hidden");
+  console.log("click");
+  document.getElementById("hamburger-menu").classList.toggle("hidden");
+}
